518d83a1b45bdab1223d5c8828cbf174cd7da40f
fcasalen <fcasalen@gmail.com>
2024-04-15 22:43:42 -0300

    unquoting try

--- file_handler/ppt_handler.py
+++ file_handler/ppt_handler.py
@@ -17,19 +17,19 @@ def extract_text_from_ppt(ppt_file):
 class PPTHandler:
     def load(file_path:str, encoding:str):
         slides = {}
-        # try:
-        presentation = Presentation(file_path)
-        text = []
-        for numSlide, slide in tqdm(enumerate(presentation.slides, start=1), desc=f'Extracting slides from ppt file {file_path}'):
-            for shape in slide.shapes:
-                if shape.has_text_frame:
-                    for paragraph in shape.text_frame.paragraphs:
-                        for run in paragraph.runs:
-                            text.append(run.text)
-            slides[f'Slide {numSlide}'] = '\n'.join(text)
-        return slides
-        # except Exception as error:
-        #     return error_loading(file_path, error=error)
+        try:
+            presentation = Presentation(file_path)
+            text = []
+            for numSlide, slide in tqdm(enumerate(presentation.slides, start=1), desc=f'Extracting slides from ppt file {file_path}'):
+                for shape in slide.shapes:
+                    if shape.has_text_frame:
+                        for paragraph in shape.text_frame.paragraphs:
+                            for run in paragraph.runs:
+                                text.append(run.text)
+                slides[f'Slide {numSlide}'] = '\n'.join(text)
+            return slides
+        except Exception as error:
+            return error_loading(file_path, error=error)
 
     def write(file_path:str, encoding:str, data:dict[str, str]):
         TxtData(data=data)
