94cc865bd7925d661bc5ceafb46fb77ff6402809
fcasalen <fcasalen@gmail.com>
2025-01-03 15:33:16 -0300

    version 0.1.10: function to check for permissionerrors and wait until is ok when writing files and tests for utils module

--- file_handler/utils.py
+++ file_handler/utils.py
@@ -1,6 +1,7 @@
 from os.path import splitext
 from datetime import datetime
-from re import match, sub
+import time
+import re
 
 def get_ext(file_path:str, valid_keys:dict):
     _,ext = splitext(file_path.lower())
@@ -42,7 +43,7 @@ def adjust_phrases(page_text:str):
         current_line = lines[i].strip()
         if current_line != '' and i < len(lines) - 1:
             next_line = lines[i + 1].strip()
-            while not (current_line.endswith(('.', '!', '?')) and match(r'^[A-Z0-9"“]', next_line)) and i < len(lines) - 2 and next_line != '':
+            while not (current_line.endswith(('.', '!', '?')) and re.match(r'^[A-Z0-9"“]', next_line)) and i < len(lines) - 2 and next_line != '':
                 if current_line[-1] != '-':
                     current_line += ' ' + next_line
                 else:
@@ -53,11 +54,35 @@ def adjust_phrases(page_text:str):
                 next_line = lines[i + 1].strip()
         joined_lines.append(current_line)
         i += 1
-    text = sub(r"\n\n+", "\n\n", '\n'.join(joined_lines))
-    text = sub(" +", ' ', text)
-    text = sub('\n ', '\n', text)
-    text = sub(' \n', '\n', text)
+    text = re.sub(r"\n\n+", "\n\n", '\n'.join(joined_lines))
+    text = re.sub(r"-\n\n", "", text)
+    text = re.sub(r"-\n", "", text)
+    text = re.sub(" +", ' ', text)
+    text = re.sub('\n ', '\n', text)
+    text = re.sub(' \n', '\n', text)
     if text != '':
         while text[-1] == '\n':
             text = text[:-1]
     return text
+
+def verify_file_is_accessible(file_path:str):
+    """
+    Verify if a file is accessible, retrying in case of PermissionError.
+
+    Args:
+        file_path (str): Path to the file to check.
+    """
+    acessible = False
+    retries = 0
+    while not acessible:
+        try:
+            with open(file_path, mode='r'):
+                pass
+            acessible = True
+        except PermissionError:
+            new_time_sleep = (retries // 10) * 2 + 2
+            print(f'File {file_path} is unaccessible. Verify! We will try again in {new_time_sleep} seconds (already tried {retries + 1} times)!')
+            time.sleep(new_time_sleep)
+            retries += 1
+        except Exception as e:
+            raise ValueError(f"An unexpected error occurred: {e}")
